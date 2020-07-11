38715356adf2389baf88a9434825ab82132e4fff
sangown2214 <ssw1333@naver.com>
2020-07-11 20:08:02 +0900

    req n res

--- server.js
+++ server.js
@@ -3,6 +3,16 @@
 const express = require('express'); // 상수화
 const app = express();
 
+
+app.use((req, res) => {
+    res.json({
+        msg: "Successful main data"
+    })
+})
+
+
+
+
 const app_port = 8099;
 
 app.listen(app_port, console.log("Server Start"));
